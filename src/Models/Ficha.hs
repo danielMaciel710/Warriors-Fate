b5c042ac849b4ec4384f2838127e60e41ff39857
Talesrib <talespiano@gmail.com>
2022-11-28 23:17:13 -0300

    corrige falha dano menor que armadura

--- src/Models/Ficha.hs
+++ src/Models/Ficha.hs
@@ -13,4 +13,6 @@ module Models.Ficha where
     arma:: (String, Int, Int),
     pocao:: Int
   } deriving (Show, Read)
-  damage ficha dano = Ficha ((vida ficha) - (dano - (armadura ficha))) (forca ficha) (destreza ficha) (constituicao ficha) (inteligencia ficha) (mana ficha) (defesa ficha) (stamina ficha) (experiencia ficha) (armadura ficha) (arma ficha) (pocao ficha)
+  damage ficha dano = if (dano > (armadura ficha)) then 
+      Ficha ((vida ficha) - (dano - (armadura ficha))) (forca ficha) (destreza ficha) (constituicao ficha) (inteligencia ficha) (mana ficha) (defesa ficha) (stamina ficha) (experiencia ficha) (armadura ficha) (arma ficha) (pocao ficha)
+    else ficha
