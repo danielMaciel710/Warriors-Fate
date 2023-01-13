2c0f399ae846fbd463534a719d72b06166bce70d
matheusinoc <matheusinocs@gmail.com>
2023-01-13 15:26:45 -0300

    gameMenu.pl e lib.pl prontos

--- gameMenu.pl
+++ gameMenu.pl
@@ -3,17 +3,25 @@
 iniciar:- mainMenu.
 
 mainMenu:-
-   writeln("___  __  ___ _    ____  ____  _  _____  ____   ___      _____  _  _____  _____"),
-   writeln("\\  \\/  \\/  // \\  |    ||    || ||  _  ||    | /___|    |  ___|/ \\|_   _||  ___|"),
-   writeln(" \\   /\\   // _ \\ |    ||    || || |_| ||    ||__  |    |  _| / _ \\ | |  |  ___|"),
-   writeln("  \\_/  \\_//_/ \\_\\|_|\\_\\|_|\\_\\|_||_____||_|\\_\\|___/     |_|  /_/ \\_\\|_|  |_____|"),
-   writeln("\n\nDigite *start* ou *help*.\n\n"),
+   writeln('___  __  ___ _    ____  ____  _  _____  ____   ___      _____  _  _____  _____'),
+   writeln('\\  \\/  \\/  // \\  |    ||    || ||  _  ||    | /___|    |  ___|/ \\|_   _||  ___|'),
+   writeln(' \\   /\\   // _ \\ |    ||    || || |_| ||    ||__  |    |  _| / _ \\ | |  |  ___|'),
+   writeln('  \\_/  \\_//_/ \\_\\|_|\\_\\|_|\\_\\|_||_____||_|\\_\\|___/     |_|  /_/ \\_\\|_|  |_____|'),
+   writeln('\n\nDigite *start* ou *help*.\n\n'),
    ler(Opcao),
    selecionar(Opcao).
 
-selecionar(help):- text("Ajuda").
+selecionar(start):- text('Início').
 
-selecionar(start):- text("Início").
+selecionar(help):- text('Help'),
+   text('Todas as ações do jogador serão através da entrada de palavras ou frases. O jogador poderá se mover, atacar, usar habilidades específicas e interagir com o cenário de forma bem simples.\n\nNas batalhas aparecerá os valores dos atributos do inimigo ou do jogador dependendo se ele usou uma habilidade ou o inimigo tenha usado.\n\nPara uma melhor experiência do jogo, ajuste a tela apertando (Ctrl + Shift + ) ou (Ctrl - ) até os traços que dividem o texto preencherem totalmente a tela sem passar para linha de baixo'),
+   text('Digite *voltar* ou *sair*'),
+   ler(Opcao),
+   selecionar(Opcao).
 
-selecionar(_):- text("Entrada Inválida"),
+selecionar(voltar):- mainMenu.
+
+selecionar(sair):- exit.
+
+selecionar(_):- text('Entrada Inválida'),
    mainMenu.
