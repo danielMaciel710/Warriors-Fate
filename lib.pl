0f85c8d6c8987a52243d911573eae6fda35a8697
matheusinoc <matheusinocs@gmail.com>
2023-01-15 12:16:06 -0300

    Ajustes em lib.pl

--- lib.pl
+++ lib.pl
@@ -4,30 +4,13 @@ ler(Opcao):- read_line_to_codes(user_input, R),
 
 ler().
 
-text(Texto):- string_length(Texto, Tamanho),
-   quebraTexto(Texto, Tamanho, R),
-   string_concat('\n-------------------------------------------------------------------------------\n', R, TextoFormatadoIncompleto),
+text(Texto):- string_concat('\n-------------------------------------------------------------------------------\n', R, TextoFormatadoIncompleto),
    string_concat(TextoFormatadoIncompleto, '\n-------------------------------------------------------------------------------\n', TextoFormatado),
    writeln(TextoFormatado).
 
+gameOver:- text('Game Over\nYour Fate were determined').
 
-quebraTexto(Texto, Tamanho, Texto):- Tamanho < 80.
-
-quebraTexto(Texto, Tamanho, TextoQuebrado):- Tamanho >= 80,
-   string_chars(Texto, R),
-   addParagrafo(' ', R, ListaR, 0),
-   atomics_to_string(ListaR, TextoQuebrado).
-
-addParagrafo(_, [], [], _).
-addParagrafo(' ', [' '|Lista], ['\n'|ListaR], N):- N > 63, addParagrafo(' ', Lista, ListaR, 0).
-addParagrafo(' ', [X|Lista],  [X|ListaR], N):- N1 is N + 1, addParagrafo(' ', Lista, ListaR, N1).
-
-gameOver:- 
-   text('Game Over\nYour Fate were determined').
-
-continuar:-
-   text('Aperte Enter para continuar...'),
+continuar:- text('Aperte Enter para continuar...'),
    ler(_).
    
-exit:-
-   text('Encerrando...').
+exit:- text('Encerrando...').
