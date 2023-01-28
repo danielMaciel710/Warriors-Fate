ff63406eb9a8ae820a662e6628384b916a9f0962
matheusinoc <matheusinocs@gmail.com>
2023-01-28 16:07:42 -0300

    Batalha Beta

--- batalha.pl
+++ batalha.pl
@@ -1,5 +1,29 @@
-batalha(Perigo, Vida):- Perigo > 2, checkHero2, batalha1.
-batalha(_):- text('Estranho, você teve a sensação que haveria algum inimigo aqui...').
+batalha(Perigo, Vida):- Perigo > 2, 
+    text('Um inimigo aparece e a batalha começa!'),
+    write('Sua vida:'),
+    writeln(Vida),
+    write('Vida do Esqueleto:'),
+    writeln(50),
+    batalhaEsqueleto(0, Vida, 50).
+batalha(_, _):- text('Estranho, você teve a sensação que haveria algum inimigo aqui...').
 
-ganharVida(Vida, NewVida):- NewVida is Vida + 10.
-batalha1:- writeln('Batalha contra um esqueleto').
+batalhaEsqueleto(_, VidaHeroi, _):- VidaHeroi < 0, fimJogo.
+batalhaEsqueleto(_, _, VidaEsqueleto):- VidaEsqueleto < 0, text('Você venceu!').
+
+batalhaEsqueleto(0, VidaHeroi, VidaEsqueleto):-
+    text('Opções: 1 = socar'),
+    ler(Opcao),
+    NewVidaEsqueleto is VidaEsqueleto - 30,
+    write('Sua vida:'),
+    writeln(VidaHeroi),
+    write('Vida do Esqueleto:'),
+    writeln(NewVidaEsqueleto),
+    batalhaEsqueleto(1, VidaHeroi, NewVidaEsqueleto).
+
+batalhaEsqueleto(1, VidaHeroi, VidaEsqueleto):- NewVidaHeroi is VidaHeroi - 25,
+    text('Esqueleto te soca.'),
+    write('Sua vida:'),
+    writeln(NewVidaHeroi),
+    write('Vida do Esqueleto:'),
+    writeln(VidaEsqueleto),
+    batalhaEsqueleto(0, NewVidaHeroi, VidaEsqueleto).
