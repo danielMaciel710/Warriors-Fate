2fc9150e0d3b2b2763f2f09170d91887d750a1a0
matheusinoc <matheusinocs@gmail.com>
2023-01-28 16:10:57 -0300

    Implementação simples de batalha

--- batalha.pl
+++ batalha.pl
@@ -1,3 +1,11 @@
+batalha(Perigo):- Perigo > 2, checkHero2, batalha1.
+batalha(_):- text('Estranho, você teve a sensação que haveria algum inimigo aqui...').
+
+checkHero2:- writeln('Verificar aqui a vida do personagem').
+batalha1:- writeln('Batalha contra um esqueleto').
+
+%Segue abaixo uma implementação bem simples de batalha
+
 batalha(Perigo, Vida):- Perigo > 2, 
     text('Um inimigo aparece e a batalha começa!'),
     write('Sua vida:'),
